7d666833ad56cd3268518c128d141bcad66b275f
Bao Ton <baothatthienton@yahoo.com>
2022-06-14 14:59:56 -0400

    make it better

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.render(<App />, document.querrySelector("#root"));
